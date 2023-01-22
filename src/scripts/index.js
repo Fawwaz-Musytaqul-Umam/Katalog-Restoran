567794da890b2ab523f980a4f3d02138b2efaf5d
Fawwaz-Musytaqul-Umam <sayafawwazinifawwaz@gmail.comgit commitgit config --global user.email sayafawwazinifawwaz@gmail.com>
2023-01-22 18:08:45 +0700

    Menambahkan Header/Hero

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,11 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/responsive.css';
 
-console.log('Hello Coders! :)');
+const menu = document.querySelector('#menuButton');
+const appBar = document.querySelector('.app-bar')
+const drawer = document.querySelector('#drawer');
+
+menu.addEventListener('click', function () {
+    drawer.classList.toggle('open')
+})
