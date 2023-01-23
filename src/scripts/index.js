448bc3a1ec58df674ec7ae54d51b99fcb04ca387
Fawwaz-Musytaqul-Umam <sayafawwazinifawwaz@gmail.comgit commitgit config --global user.email sayafawwazinifawwaz@gmail.com>
2023-01-23 16:05:09 +0700

    Memperbaiki Hero

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,15 +3,20 @@ import '../styles/main.css';
 import '../styles/responsive.css';
 
 const menu = document.querySelector('#menuButton');
-const heroInner = document.querySelector('.hero-inner')
+const heroInner = document.querySelector('.hero-inner');
 const drawer = document.querySelector('#drawer');
+const main = document.querySelector('main');
 
 menu.addEventListener('click', function () {
-    drawer.classList.toggle('open')
+	drawer.classList.toggle('open');
 
-    if (drawer.classList.contains('open')) {
-        heroInner.style.display = 'none';
-    } else {
-        heroInner.style.display = 'block';
-    }
+	if (drawer.classList.contains('open')) {
+		heroInner.style.display = 'none';
+	} else {
+		heroInner.style.display = 'block';
+	}
+});
+
+main.addEventListener('click', function () {
+    drawer.classList.remove('open');
 })
